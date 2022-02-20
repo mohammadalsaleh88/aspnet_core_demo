0e54cfa818388a2227e262da88af63b39d546eaf
mohammadalsaleh88 <mohammad.alsaleh88@outlook.com>
2022-02-20 09:33:47 +0100

    Create new apn.net core project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aspnet_core_demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
