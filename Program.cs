293e7af06a8a6d82061f6d7c32a70eb1996283b2
mohammadalsaleh88 <47926572+mohammadalsaleh88@users.noreply.github.com>
2023-11-16 22:44:44 +0100

    Added support for asp.net metrics

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 // using OpenTelemetry.Metrics;
 
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews(o =>
 o.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
 
-// builder.Services.AddOpenTelemetry()
-//     .WithMetrics(builder =>
-//     {
-//         builder.AddPrometheusExporter();
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
 
-//         builder.AddMeter("Microsoft.AspNetCore.Hosting",
-//                          "Microsoft.AspNetCore.Server.Kestrel");
-//         builder.AddView("http.server.request.duration",
-//             new ExplicitBucketHistogramConfiguration
-//             {
-//                 Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
-//                        0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
-//             });
-//     });
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+    });
 
 var app = builder.Build();
 
@@ -38,7 +40,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-// app.MapPrometheusScrapingEndpoint();
+app.MapPrometheusScrapingEndpoint();
 
 app.MapControllerRoute(
     name: "default",
