6a438168113272d72679f82cf8f882bdb8ede82c
mohammadalsaleh88 <47926572+mohammadalsaleh88@users.noreply.github.com>
2023-11-20 21:07:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -50,7 +50,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.MapPrometheusScrapingEndpoint();
+    //app.MapPrometheusScrapingEndpoint();
 
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios
